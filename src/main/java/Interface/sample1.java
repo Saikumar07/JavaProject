f0061acf9322de2b52827d6e6b9bd14d2284ceea
Saikumar07 <beemanasai7@gmail.com>
2018-10-23 23:32:50 +0530

    Modified sample1

--- src/main/java/Interface/sample1.java
+++ src/main/java/Interface/sample1.java
@@ -3,7 +3,10 @@ package Interface;
 public class sample1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("print c value "+c);
 		
 	}
 
