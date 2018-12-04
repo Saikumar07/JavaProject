3147a8568ea6a96f1f5f998573620358f6bc33f2
Saikumar07 <beemanasai7@gmail.com>
2018-12-04 20:50:26 +0530

    Modified testcases

--- src/main/java/Interface/sample1.java
+++ src/main/java/Interface/sample1.java
@@ -6,7 +6,8 @@ public class sample1 {
 		int a=10;
 		int b=20;
 		int c=a+b;
-		System.out.println("print c value "+c);		
+		System.out.println("print c value "+c);
+		System.out.println("print c value "+c);
 	}
 
 }
